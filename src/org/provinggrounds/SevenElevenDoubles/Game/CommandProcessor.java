2e8d1d155b88f282994bf0362849e8e160b7c37b
Sandeep Raghuraman <sandy.8925@gmail.com>
2014-03-21 20:49:39 -0700

    Add new class CommandProcessor

---
+++ src/org/provinggrounds/SevenElevenDoubles/Game/CommandProcessor.java
@@ -0,0 +1,15 @@
+package org.provinggrounds.SevenElevenDoubles.Game;
+
+/**
+ * Should be used to process user commands. Call method processCommand().
+ */
+public final class CommandProcessor {
+
+    /**
+     * Process user's commands
+     * @param userCommand Command to be processed. Pass entire command line in a single String.
+     */
+    public static void processCommand(String userCommand) {
+        //stub
+    }
+}
