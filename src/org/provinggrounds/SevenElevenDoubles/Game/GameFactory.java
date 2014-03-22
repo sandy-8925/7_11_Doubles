ddec55eee17cc9cd0bdd27b65d3363e89c9bcfec
Sandeep Raghuraman <sandy.8925@gmail.com>
2014-03-21 20:59:59 -0700

    Add class GameFactory

---
+++ src/org/provinggrounds/SevenElevenDoubles/Game/GameFactory.java
@@ -0,0 +1,17 @@
+package org.provinggrounds.SevenElevenDoubles.Game;
+
+/**
+ * Used to hold game properties during command input stage.
+ * When game is ready to be started, can be asked to construct game object.
+ */
+public final class GameFactory {
+
+    /**
+     * Informs caller whether game start has been signalled.
+     * @return True if game can be started, and 'start' command has been issued. False otherwise.
+     */
+    public static boolean gameStartSignalled() {
+        //stub
+        return true;
+    }
+}
