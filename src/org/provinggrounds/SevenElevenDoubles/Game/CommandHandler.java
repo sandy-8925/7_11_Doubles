30a00db25764c72e5c311f76570ac1c4655ad29c
Sandeep Raghuraman <sandy.8925@gmail.com>
2014-03-29 11:22:28 -0700

    Rename interface Command to CommandHandler

--- src/org/provinggrounds/SevenElevenDoubles/Game/CommandHandler.java
+++ src/org/provinggrounds/SevenElevenDoubles/Game/CommandHandler.java
@@ -6,7 +6,7 @@ import java.util.List;
  *  A common interface for individual command handlers.
  *  (e.g) 'help' command will have one handler, 'speed' command another handler
  */
-interface Command {
+interface CommandHandler {
     /**
      * Perform the operations for this command
      * @param arguments A collection of arguments for this command, in the order entered by the user.
