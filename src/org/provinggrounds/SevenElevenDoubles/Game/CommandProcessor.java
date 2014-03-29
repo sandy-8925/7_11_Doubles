7a15c89070fa3292e7d3be170f7e7a4d03455485
Sandeep Raghuraman <sandy.8925@gmail.com>
2014-03-29 11:31:20 -0700

    CommandProcessor: Add/change code to parse user commands, extract command and arguments and execute the command

--- src/org/provinggrounds/SevenElevenDoubles/Game/CommandProcessor.java
+++ src/org/provinggrounds/SevenElevenDoubles/Game/CommandProcessor.java
@@ -1,17 +1,23 @@
 package org.provinggrounds.SevenElevenDoubles.Game;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 /**
  * Should be used to process user commands. Call method processCommand().
  */
 public final class CommandProcessor {
 
+    private static Map<String, CommandHandler> commandMap;
+
+    static {
+        //initialize commandMap
+        commandMap = new HashMap<>();
+        commandMap.put("help", new HelpCommandHandler());
+    }
+
     /**
      * Process user's commands
-     * @param userCommand Command to be processed. Pass entire command line in a single String.
+     * @param userCommand CommandHandler to be processed. Pass entire command line in a single String.
      *                    If null, empty or invalid, no action is taken
      */
     public static void processCommand(String userCommand) {
@@ -22,13 +28,23 @@ public final class CommandProcessor {
         String[] commandWords = userCommand.split(" ", 2);
         if(commandWords.length < 1)
             return;
-        String command = commandWords[0];
+        String command = commandWords[0].toLowerCase(); //input command should be recognized independent of case
         List<String> arguments = Collections.emptyList();
         if(commandWords.length >= 2) {  //arguments exist
-            arguments.addAll(Arrays.asList(commandWords[1].split(" ")));
+            arguments = Arrays.asList(commandWords[1].split(" "));
+        }
+
+        //filter out empty strings in arguments
+        ListIterator<String> argsIterator = arguments.listIterator();
+        while(argsIterator.hasNext()) {
+            if(argsIterator.next().isEmpty())
+                argsIterator.remove();
         }
 
         //Examine first word - if valid command word, execute command
+        if(commandMap.containsKey(command)) {
+            commandMap.get(command).execute(arguments);
+        }
 
         //Else return without doing anything
     }
